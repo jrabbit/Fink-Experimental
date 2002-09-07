af54d4685d740a3f798e432d362710f4110ad09e
mrvacbob <mrvacbob>
2002-09-07 01:13:08 +0000

    Removing the extranenous colons from the output

--- mrvacbob/dpkg-checkall.sh
+++ mrvacbob/dpkg-checkall.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-for foo in `cat /sw/var/lib/dpkg/info/*.list | uniq`
+for foo in `cat /sw/var/lib/dpkg/info/*.list`
 do
-if (! test -e $foo)
+if (! test -e "$foo")
 then
 echo $foo not found
-echo $foo is part of `dpkg -S $foo | awk '{print $1}'`
+echo $foo is part of `dpkg -S $foo | awk '{print $1}' | sed -e 's;:;;g' | sort | uniq`
 fi
 done
