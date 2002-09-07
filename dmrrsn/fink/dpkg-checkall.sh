6f1217c08442a3e14f4fc5a9c90c4099055371e6
dmrrsn <dmrrsn>
2002-09-07 15:21:29 +0000

    more scripts for the update

---
+++ dmrrsn/fink/dpkg-checkall.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# dpkg-checkall.sh -- A script to check if everything which dpkg thinks
+#                     is installed, is actually installed.  Outputs a
+#                     list of packages with problems.
+#
+#                     Script written by Alexander Strange.
+#
+# Fink - a package manager that downloads source and installs it
+# Copyright (c) 2001 Christoph Pfisterer
+# Copyright (c) 2001-2002 The Fink Package Manager Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
+fink_path=`which dpkg | sed -e 's:/bin/dpkg::'`
+for foo in `cat $fink_path/var/lib/dpkg/info/*.list`
+do
+if (! test -e "$foo")
+then
+echo $foo not found
+echo $foo is part of `dpkg -S $foo | awk '{print $1}' | sed -e 's;:;;g' | sort | uniq`
+fi
+done
