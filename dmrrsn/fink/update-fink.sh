607b2627b3d04c48fdd24717806dd2ff12971eda
dmrrsn <dmrrsn>
2002-09-08 11:59:55 +0000

    script revisions

--- dmrrsn/fink/update-fink.sh
+++ dmrrsn/fink/update-fink.sh
@@ -21,18 +21,27 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
+# Remove two packages which will interfere with the update if they aren't
+# at the latest versions
 fink remove openldap-ssl cyrus-sasl
 
+# Force-remove a disfunctional package which won't be used in 10.2
 sudo dpkg -r --force-depends manconf
 
+# Create Packages.gz and Release files for the last time in 10.1 directories
+# before they are moved to another location (so that apt-get can find them
+# even after the upgrade)
 fink scanpackages
 
+# Install the new version of fink, whose post-install script modifies the
+# directory structure for 10.2
 ./inject.pl
 
+# Finish the setup with fink selfupdate-cvs, fink scanpackages, and apt-get
+# update
+
 fink selfupdate-cvs
-
 fink scanpackages
-
 sudo apt-get update
 
 exit 0
