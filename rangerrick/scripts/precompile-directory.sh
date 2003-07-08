70a5df2ccac997b8f8ec0b2944d0e096dc45ed42
rangerrick <rangerrick>
2003-07-08 00:58:46 +0000

    [[ ]] for a bit of speed also touch a file to speed things up (find only newer than the touch file if it exists)

--- rangerrick/scripts/precompile-directory.sh
+++ rangerrick/scripts/precompile-directory.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
-if test "$1" = "-v"; then
+if [[ "$1" = "-v" ]]; then
 	VERBOSE=true
 	shift
 fi
 
-if test -z "$1"; then
+if [[ -z "$1" ]]; then
 	echo "usage: $0 [-v] [directory1..directoryN]"
 	exit 1
 fi
 
 CACHEDIR=/tmp/pch
+if [[ -f "$CACHEDIR/last-updated" ]]; then
+	NEWER="-newer $CACHEDIR/last-updated"
+fi
 
 INCLUDEDIRS=`find "$@" -type d -a ! -name \*.gch -exec echo -I{} \; | xargs echo`
-find "$@" -name \*.h -type f | while read HEADER; do
-	if test -f "$HEADER.gch"; then
+find "$@" -name \*.h -type f $NEWER | while read HEADER; do
+	if [[ -f "$HEADER.gch" ]]; then
 		sudo rm -rf "$HEADER.gch"
 	fi
 	mkdir -p "$HEADER.gch"
@@ -23,28 +26,30 @@ find "$@" -name \*.h -type f | while read HEADER; do
 	for type in "gcc:c" "g++:c++"; do
 		COMPILER=`echo $type | cut -d: -f1`
 		FILENAME=`echo $type | cut -d: -f2`
-		if [ "$VERBOSE" = "true" ]; then
+		if [[ "$VERBOSE" = "true" ]]; then
 			echo -e "generating $HEADER.gch/$FILENAME.pch \c"
 		fi
 		transname=`echo $HEADER.gch/$FILENAME.pch | sed -e 's,//*,_,g'`
-		if [ -f "$CACHEDIR/$transname" ] && ! [ -s "$HEADER.gch/$FILENAME.pch" ]; then
-			if [ "$VERBOSE" = "true" ]; then
+		if [[ -f "$CACHEDIR/$transname" ]] && ! [[ -s "$HEADER.gch/$FILENAME.pch" ]]; then
+			if [[ "$VERBOSE" = "true" ]]; then
 				echo "previously failed"
 			fi
-		elif [ "$HEADER" -nt "$HEADER.gch/$FILENAME.pch" ]; then
+		elif [[ "$HEADER" -nt "$HEADER.gch/$FILENAME.pch" ]]; then
 			if $COMPILER-no-cpp-precomp $INCLUDEDIRS -o $HEADER.gch/$FILENAME.pch $HEADER >/dev/null 2>&1 || \
 				$COMPILER $INCLUDEDIRS -o $HEADER.gch/$FILENAME.pch $HEADER >/dev/null 2>&1; then
-				if [ "$VERBOSE" = "true" ]; then
+				if [[ "$VERBOSE" = "true" ]]; then
 					echo "done"
 				else
 					echo "$HEADER precompiled for $FILENAME"
 				fi
 			else
-				[ "$VERBOSE" = "true" ] && echo "failed"
+				[[ "$VERBOSE" = "true" ]] && echo "failed"
 			fi
 		else
-			[ "$VERBOSE" = "true" ] && echo "exists"
+			[[ "$VERBOSE" = "true" ]] && echo "exists"
 		fi
 		touch "$CACHEDIR/$transname"
 	done
 done
+
+touch "$CACHEDIR/last-updated"
