d025e21451dd3af6614cac37e92439ee5c66d8b6
rangerrick <rangerrick>
2003-08-21 12:01:01 +0000

    fixed i18n files

--- rangerrick/finkinfo/kde/generate-i18n.pl
+++ rangerrick/finkinfo/kde/generate-i18n.pl
@@ -5,7 +5,7 @@ my $KDEVERSION    = '3.1.3';
 my $KDEDIRECTORY  = 'stable/%v/src/';
 my $KDERELNUM     = 1;
 my $KDEARTSVER    = '1.1.3-1';
-my $KDEI18NRELNUM = 1;
+my $KDEI18NRELNUM = 2;
 my $KOVERSION     = '1.2.92';
 my $KODIRECTORY   = 'unstable/koffice-%v/src/';
 my $KORELNUM      = '1';
@@ -50,7 +50,7 @@ for my $i18n (@KDEI18N) {
 		my $contents = <<END;
 Package: kde-i18n-${normalized}
 Source: mirror:kde:${KDEDIRECTORY}kde-i18n/${filename}
-SourceDirectory: kde-i18n-${shortname}
+SourceDirectory: kde-i18n-${shortname}-%v
 Description: KDE - language files for $MAPPINGS{$shortname}
 DescDetail: Language files for the K Desktop Environment: $MAPPINGS{$shortname}
 Source-MD5: $md5
