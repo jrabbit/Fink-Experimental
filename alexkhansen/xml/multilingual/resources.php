ed06164107b4fe439cbcc56d845474393bdc0123
alexkhansen <alexkhansen>
2004-03-04 19:13:52 +0000

    some changes

---
+++ alexkhansen/xml/multilingual/resources.php
@@ -0,0 +1,39 @@
+<?
+$title = "i18n - resources";
+$cvs_author = 'Author: alexkhansen';
+$cvs_date = 'Date: 2004/03/04 02:17:05';
+
+$metatags = '<link rel="contents" href="index.php" title="i18n Contents"><link rel="prev" href="procedure.php" title="Procedure for Updating Documents">';
+
+include "header.inc";
+?>
+
+<h1>i18n - 4 Additional Resources.</h1>
+    
+    
+    <h2><a name="resources">4.1 </a></h2>
+      <p>
+	Internationalization is a complicated subject. The resources provided below can serve as a valuable second source of information. You are advised to read them carefully if you wish to delve deeper into this subject.
+	</p>
+      <ol>
+        <li>
+          <a href="http://www.w3.org/TR/unicode-xml/">Unicode in XML and other Markup Languages</a>
+        </li>
+        <li>
+          <a href="http://www.w3.org/International/tutorials/tutorial-char-enc.html">Tutorial: Character sets &amp; encodings</a>
+        </li>
+        <li>
+          <a href="http://www.w3.org/International/">W3C Internationalization Activity</a>
+        </li>
+        <li>
+          <a href="http://www.cl.cam.ac.uk/~mgk25/unicode.html">UTF-8 and Unicode FAQ for Unix/Linux</a>
+        </li>
+      </ol>
+    
+  
+
+
+<?
+include "footer.inc";
+?>
+
