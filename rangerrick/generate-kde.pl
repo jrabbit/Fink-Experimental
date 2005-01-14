e524f008680e0561e293a31ada02a804824b44d5
rangerrick <rangerrick>
2005-01-14 22:19:34 +0000

    freetype needs updating... some versions of libtool expect the .la to be wherever "libdir" is pointing inside the .la file

--- rangerrick/generate-kde.pl
+++ rangerrick/generate-kde.pl
@@ -72,7 +72,7 @@ for my $file (sort keys %files) {
 									$line =~ s/libgsf-dev/libgsf/g;
 									$line =~ s/gstreamer\S*?(\s+\([^\)]+\))?\s*[\,\s]*//g;
 									$line =~ s/((imagemagick.*?)-dev)/$2/g;
-									$line =~ s/(freetype2\S*) \((\S+)\s+2.1.3-22\)/$1 ($2 2.1.3-2)/g;
+									$line =~ s/(freetype2\S*) \((\S+)\s+2.1.3-23\)/$1 ($2 2.1.3-2)/g;
 								} else {
 									$line =~ s/^\#(\s*export\s+LD_TWOLEVEL_NAMESPACE.*)$/$1/;
 									$line =~ s/(dlcompat|libpoll)(\S*)(\s+\([^\)]+\))?[\,\s]*//g unless ($line =~ m#-I/usr/X11R6/include#);
