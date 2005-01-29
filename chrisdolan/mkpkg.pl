2316de72e4febca09acaa2d579c1d75f105ce079
chrisdolan <chrisdolan>
2005-01-29 05:06:30 +0000

    .info file auto-builder for Perl modules -- first working version
    I used this to auto-create the crypt-rc4-pm.info file (less a few
    hand-tweaks)

--- chrisdolan/mkpkg.pl
+++ chrisdolan/mkpkg.pl
@@ -1,13 +1,31 @@
 #!/usr/bin/perl -w
 
-# Create .info files for CPAN perl modules
-## UNFINISHED.  Right now, this just collects info via CPANPLUS
+# Create first-cut .info file for CPAN perl module
+#    Syntax: mkpkg.pl -m "Joe Maintainer <joe@foo.com>" Some::Module
+# if you specify more than one module, it will process them sequentially
+#   Args:
+#     -v   verbose
+#     -m   specify the maintainer
+#     -p   show prereq info (not yet finished)
+#     -f   force overwrite of .info file
+#     -h   help (not yet implemented)
+
+# Caveats:
+#   Does not do Depends, Recommends, BuildDepends, etc
+#   Does not do variants or SplitOffs
+#   Just guesses at DocFiles
+#   License may not be right - confirm manually
+#   Does not indicate crypto or main dependencies
+
+# Note: requires CPANPLUS 0.051 to be installed
+#       untested with other versions of CPANPLUS
+# Note: requires file-slurp-pm.info to be installed
 
 use strict;
 use Getopt::Long;
 use CPANPLUS::Backend;
 use CPANPLUS::Internals::Constants;
-
+use File::Slurp;
 use Carp;
 $SIG{__WARN__} = $SIG{__DIE__} = \&Carp::confess;
 
@@ -16,6 +34,10 @@ my %opts = (
             readonly   => 0,
             help       => 0,
             version    => 0,
+            force      => 0,
+            dir        => "/sw/fink/dists/local/main/finkinfo/libs/perlmods",
+            maintainer => "Chris Dolan <chrisdolan\@users.sourceforge.net>",
+            prereqs    => 0, # not finished
             );
 
 Getopt::Long::Configure("bundling");
@@ -23,7 +45,20 @@ GetOptions("v|verbose"  => \$opts{verbose},
            "r|readonly" => \$opts{readonly},
            "h|help"     => \$opts{help},
            "V|version"  => \$opts{version},
+           "f|force"    => \$opts{force},
+           "m|maintainer=s" => \$opts{maintainer},
+           "p|prereqs"  => \$opts{prereqs},
            ) or die;
+
+# This is a translation from CPAN "dslip" codes to Fink license words
+my %licenses = (
+                p   => "Artistic/GPL",
+                g   => "GPL",
+                l   => "LGPL",
+                b   => "BSD",
+                a   => "Artistic",
+                o   => "Restrictive/Distributable",
+                );
 
 my $cb = CPANPLUS::Backend->new();
 print "Started CPANPLUS\n";
@@ -35,24 +70,91 @@ foreach my $module (@ARGV)
    if ($mod)
    {
       print "Found\n";
-      #print join(".",
-      #           $mod->package_name, 
-      #           $mod->package_version, 
-      #           $mod->package_extension)."\n";
-      my $details = $mod->details;
-      for my $key (keys %$details)
+
+      my $pkg = lc($mod->package_name)."-pm";
+      my $file = "$opts{dir}/$pkg.info";
+      if (-f $file && !$opts{force})
       {
-         print "$key: $$details{$key}\n";
+         print "info package already exists\n";
+         print "$file\n";
+         next;
       }
-      $mod->status->fetch || $mod->fetch;
-      $mod->status->extract || $mod->extract;
-      my $dist = $mod->dist(format => $mod->get_installer_type(),
-                            target => TARGET_PREPARE);
-      my $prereqs = $dist->_find_prereqs();
-      for my $key (sort keys %$prereqs)
+
+      if ($opts{verbose})
       {
-         print "  Prereq: $key => $$prereqs{$key}\n";
+         my $details = $mod->details;
+         for my $key (keys %$details)
+         {
+            print "$key: $$details{$key}\n";
+         }
       }
+
+      if (!$mod->status->fetch)
+      {
+         print "Fetch module\n";
+         $mod->fetch;
+      }
+      if (!$mod->status->extract)
+      {
+         print "Extract module\n";
+         $mod->extract;
+      }
+      print "Extracted to ".$mod->status->extract."\n";
+
+      if ($opts{prereqs})
+      {
+         my $dist = $mod->dist(format => $mod->get_installer_type(),
+                               target => TARGET_PREPARE);
+         my $prereqs = $dist->_find_prereqs();
+         for my $key (sort keys %$prereqs)
+         {
+            print "  Prereq: $key => $$prereqs{$key}\n";
+         }
+      }
+
+      # Debugging: dump the module object to a file
+      #use Data::Dumper;
+      #write_file("/tmp/module", Dumper($mod));
+
+      # license code is 5th character in "DSLIP" code
+      my $dslip = $mod->dslip;
+      my $license_code = substr($dslip, 4, 1);
+      my $license = $licenses{$license_code} || "";
+
+      # Get list of files in the root of the distro.  Some of them
+      # will be the DocFiles
+      my @files = grep {-f $_} map {$mod->status->extract."/".$_} read_dir($mod->status->extract);
+
+      # Prepare the .info fields.  Note that we use "_" instead of "-"
+      # in field names to keep perl happy.
+      my @data = (
+                  Package => $pkg,
+                  Version => $mod->package_version,
+                  Revision => "1",
+                  Source => ("mirror:cpan:" . $mod->path . "/" . 
+                             $mod->package_name . "-%v." . 
+                             $mod->package_extension),
+                  Source_MD5 => $mod->status->checksum_value,
+                  Type => "perl",
+                  UpdatePOD => "true",
+                  DocFiles => "@files",
+                  License => $license,
+                  Description => $mod->description,
+                  Maintainer => $opts{maintainer},
+                  Homepage => "http://search.cpan.org/dist/".$mod->package_name,
+                  );
+
+      # Write the .info file
+      my $output = "";
+      for (my $i=0; $i<@data; $i+=2)
+      {
+         my $label = $data[$i];
+         my $value = $data[$i+1];
+         $label =~ s/_/-/g;
+         $output .= "$label: $value\n";
+      }
+      write_file($file, $output);
+      print "Wrote file $file\n";
    }
    else
    {
@@ -60,10 +162,12 @@ foreach my $module (@ARGV)
    }
 }
 
-if ($opts{readonly})
-{
-   for (CPANPLUS::Internals->_return_all_objects())
-   {
-      CPANPLUS::Internals->_remove_id($_->_id);
-   }
-}
+## This was an experiment to speed up the cleanup phase.  But, it
+## broke the persistent caching that CPANPLUS performs...
+#if ($opts{readonly})
+#{
+#   for (CPANPLUS::Internals->_return_all_objects())
+#   {
+#      CPANPLUS::Internals->_remove_id($_->_id);
+#   }
+#}
