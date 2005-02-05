1a2c8cfe4fec97fefae01887679f0170fef27adf
chrisdolan <chrisdolan>
2005-02-05 03:41:22 +0000

    v1.00 of my program to create .info files from CPAN modules

--- chrisdolan/mkpkg.pl
+++ chrisdolan/mkpkg.pl
@@ -1,54 +1,97 @@
 #!/usr/bin/perl -w
 
-# Create first-cut .info file for CPAN perl module
-#    Syntax: mkpkg.pl -m "Joe Maintainer <joe@foo.com>" Some::Module
-# if you specify more than one module, it will process them sequentially
-#   Args:
-#     -v   verbose
-#     -m   specify the maintainer
-#     -p   show prereq info (not yet finished)
-#     -f   force overwrite of .info file
-#     -h   help (not yet implemented)
+=head1 NAME
 
-# Caveats:
-#   Does not do Depends, Recommends, BuildDepends, etc
-#   Does not do variants or SplitOffs
-#   Just guesses at DocFiles
-#   License may not be right - confirm manually
-#   Does not indicate crypto or main dependencies
+mkpkg.pl - Create a Fink info file from a CPAN module
 
-# Note: requires CPANPLUS 0.051 to be installed
-#       untested with other versions of CPANPLUS
-# Note: requires file-slurp-pm.info to be installed
+=head1 SYNOPSIS
+
+mkpkg.pl [options] Some::Module
+
+  Options:
+    -m --maintainer=s   specify the maintainer
+                        (e.g. -m "Joe Maintainer E<lt>joe@foo.comE<gt>")
+    -p --prereqs        show prereq info (not yet finished)
+    -f --force          force overwrite of .info file
+    -t --typepkg        use %type_pkg[perl] variants
+    -b --bin            force a %N-bin splitoff
+    -v --verbose        print the internal representation of the PDF
+    -h --help           verbose help message
+    -V --version        print version
+
+If you specify more than one module, they will be processed sequentially.
+
+=head1 DESCRIPTION
+
+=head1 REQUIREMENTS
+
+CPANPLUS 0.051 must be installed.  This is untested with other
+versions of CPANPLUS.
+
+file-slurp-pm.info must be installed.
+ 
+=head1 CAVEATS
+
+Does not do Depends, Recommends, BuildDepends, etc
+
+Does not do variants or SplitOffs
+
+Just guesses at DocFiles
+
+License may not be right - confirm manually
+
+Does not indicate crypto or main dependencies
+
+=head1 LICENSE
+
+This program can be distributed under the same terms as Fink.
+
+=head1 AUTHOR
+
+Chris Dolan <chrisdolan@users.sourceforge.net>
+
+=cut
 
 use strict;
 use Getopt::Long;
+use Pod::Usage;
 use CPANPLUS::Backend;
 use CPANPLUS::Internals::Constants;
 use File::Slurp;
 use Carp;
 $SIG{__WARN__} = $SIG{__DIE__} = \&Carp::confess;
 
+my $VERSION = "1.00";
 my %opts = (
             verbose    => 0,
-            readonly   => 0,
             help       => 0,
             version    => 0,
             force      => 0,
             dir        => "/sw/fink/dists/local/main/finkinfo/libs/perlmods",
             maintainer => "Chris Dolan <chrisdolan\@users.sourceforge.net>",
             prereqs    => 0, # not finished
+            bin        => 0, # make a %N-bin splitoff
+            typepkg    => 0,
+            perltypes  => "5.8.0 5.8.1 5.8.4",
             );
 
 Getopt::Long::Configure("bundling");
 GetOptions("v|verbose"  => \$opts{verbose},
-           "r|readonly" => \$opts{readonly},
            "h|help"     => \$opts{help},
            "V|version"  => \$opts{version},
            "f|force"    => \$opts{force},
            "m|maintainer=s" => \$opts{maintainer},
            "p|prereqs"  => \$opts{prereqs},
-           ) or die;
+           "t|typepkg"  => \$opts{typepkg},
+           "b|bin"      => \$opts{bin},
+           ) or pod2usage(1);
+pod2usage(-exitstatus => 0, -verbose => 2) if ($opts{help});
+print("$0 v$VERSION\n"),exit(0) if ($opts{version});
+
+if (@ARGV < 1)
+{
+   pod2usage(1);
+}
 
 # This is a translation from CPAN "dslip" codes to Fink license words
 my %licenses = (
@@ -123,20 +166,41 @@ foreach my $module (@ARGV)
 
       # Get list of files in the root of the distro.  Some of them
       # will be the DocFiles
-      my @files = grep {-f $_} map {$mod->status->extract."/".$_} read_dir($mod->status->extract);
+      my @files = map({$_->[1]}
+                      grep({-f $_->[0]}
+                           map({[$mod->status->extract."/".$_,$_]} 
+                               read_dir($mod->status->extract))));
+      @files = grep !/^(Makefile(\.PL|)|MANIFEST\.SKIP|.*\.bat|test\.pl)$/, @files;
+
+      if (-f $mod->status->extract."/Makefile.PL")
+      {
+         my $makefile = read_file($mod->status->extract."/Makefile.PL");
+         # Get ABSTRACT string from the MakeMaker command
+         if ($makefile =~ /ABSTRACT *(?:=>|,) *(?:\"([^\"]+)|\'([^\']+))/)
+         {
+            $mod->description($1);
+         }
+         # Check if there are any script outputs
+         if ($makefile =~ /EXE_FILES *(?:=>|,)/)
+         {
+            $opts{bin} = 1;
+         }
+      }
 
       # Prepare the .info fields.  Note that we use "_" instead of "-"
       # in field names to keep perl happy.
       my @data = (
-                  Package => $pkg,
+                  Package => $pkg.($opts{typepkg} ? "%type_pkg[perl]" : ""),
                   Version => $mod->package_version,
                   Revision => "1",
                   Source => ("mirror:cpan:" . $mod->path . "/" . 
                              $mod->package_name . "-%v." . 
                              $mod->package_extension),
                   Source_MD5 => $mod->status->checksum_value,
-                  Type => "perl",
+                  Type => ($opts{typepkg} ? 
+                           "perl ($opts{perltypes})" : "perl"),
                   UpdatePOD => "true",
+                  Depends => "",
                   DocFiles => "@files",
                   License => $license,
                   Description => $mod->description,
@@ -145,13 +209,66 @@ foreach my $module (@ARGV)
                   );
 
       # Write the .info file
+      my $splitoffs = 0;
       my $output = "";
+      my %data;
       for (my $i=0; $i<@data; $i+=2)
       {
          my $label = $data[$i];
          my $value = $data[$i+1];
+         if (!defined $value)
+         {
+            print "Undef field '$label'\n";
+            $value = "";
+         }
+         $data{$label} = $value;
          $label =~ s/_/-/g;
          $output .= "$label: $value\n";
+      }
+      my $manconflict = join(", ", map("%{Ni}$_-man", split(/ +/, $opts{perltypes})));
+      (my $binconflict = $manconflict) =~ s/-man/-bin/g;
+      if ($opts{bin})
+      {
+         my $splitnum = ++$splitoffs == 1 ? "" : $splitoffs;
+         if ($opts{typepkg})
+         {
+            $output .= <<"EOF";
+Splitoff$splitnum: <<
+ Package: %N-bin
+ Depends: %N (= %v-%r)
+ Files: bin
+ Conflicts: $binconflict
+ Replaces: $binconflict
+ DocFiles: $data{DocFiles}
+<<
+EOF
+         }
+         else
+         {
+            $output .= <<"EOF";
+Splitoff$splitnum: <<
+ Package: %N-bin
+ Depends: %N (= %v-%r)
+ Files: bin share/man/man1
+ DocFiles: $data{DocFiles}
+<<
+EOF
+         }
+      }
+      if ($opts{typepkg})
+      {
+         my $splitnum = ++$splitoffs == 1 ? "" : $splitoffs;
+         $output .= <<"EOF";
+Splitoff$splitnum: <<
+ Package: %N-man
+ Depends: %N (= %v-%r)
+ Files: share/man
+ Conflicts: $manconflict
+ Replaces: $manconflict
+ DocFiles: $data{DocFiles}
+<<
+EOF
+         $output = "Info2: <<\n$output<<\n";
       }
       write_file($file, $output);
       print "Wrote file $file\n";
@@ -161,13 +278,3 @@ foreach my $module (@ARGV)
       print "Not found\n";
    }
 }
-
-## This was an experiment to speed up the cleanup phase.  But, it
-## broke the persistent caching that CPANPLUS performs...
-#if ($opts{readonly})
-#{
-#   for (CPANPLUS::Internals->_return_all_objects())
-#   {
-#      CPANPLUS::Internals->_remove_id($_->_id);
-#   }
-#}
