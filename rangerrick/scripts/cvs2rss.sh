908f59034841ff41625c0981a035565d7fa5dd3a
rangerrick <rangerrick>
2005-03-21 21:34:02 +0000

    cvs2cl script

---
+++ rangerrick/scripts/cvs2rss.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+DIRNAME=`dirname $0`
+DIRNAME=`cd $DIRNAME; pwd`
+
+export CVS_RSH="$HOME/bin/ssh.sh"
+
+pushd "$DIRNAME/.." >/dev/null 2>&1
+echo "=== running 'cvs up' ==="
+cvs up
+echo "=== running cvs2cl ==="
+$DIRNAME/cvs2cl.pl --xml --stdout > ChangeLog.xml
+echo "=== running xsltproc ==="
+xsltproc filter-cvs2cl.xslt ChangeLog.xml > ChangeLog-rangerrick.rdf
+popd >/dev/null 2>&1
