02c5d9192326ba0c7fedb089641db580140e138c
asari <asari>
2005-10-20 14:57:17 +0000

    misc fix

--- asari/openoffice.org.mksplitoffs.sh
+++ asari/openoffice.org.mksplitoffs.sh
@@ -8,9 +8,9 @@ for i in `jot $nLangs 1 $nLangs`; do
    LANG=`echo $LANGS | cut -d" " -f "$i"`
    LANG_LOWERCASE=`echo $LANG | tr "[:upper:]" "[:lower:]"`
    if [ $i == 1 ]; then
-      sed -e "s/@LANG@/$LANG/g" -e "s/@LANG_LOWERCASE@/$LANG_LOWERCASE/g" openoffice.splitoff
+      sed -e "s/@LANG@/$LANG/g" -e "s/@LANG_LOWERCASE@/$LANG_LOWERCASE/g" openoffice.org.splitoff.in
    else
       sed -e "s/@LANG@/$LANG/g" -e "s/@LANG_LOWERCASE@/$LANG_LOWERCASE/g" \
-        -e "s/SplitOff:/SplitOff${i}:/"  openoffice.splitoff
+        -e "s/SplitOff:/SplitOff${i}:/"  openoffice.org.splitoff.in
    fi
 done
