e08ec8ca5085379804551725fc06e90321c17dff
rangerrick <rangerrick>
2006-01-28 20:10:39 +0000

    version requirement updates; postgresql doc fixes

--- rangerrick/scripts/gcc-symbol-mangle-detector.sh
+++ rangerrick/scripts/gcc-symbol-mangle-detector.sh
@@ -6,7 +6,7 @@ for PACKAGE in "$@"; do
 	dpkg -L "$PACKAGE" | while read FILE; do
 		if [ "$MATCH" -eq 0 ] && [ -f "$FILE" ]; then
 			if [ `file "$FILE" 2>/dev/null | grep -c "Mach-O"` -gt 0 ]; then
-				nm "$FILE" > /tmp/$$.before
+				nm -g "$FILE" > /tmp/$$.before
 				c++filt < /tmp/$$.before > /tmp/$$.after
 				COUNT=`diff /tmp/$$.before /tmp/$$.after | wc -l`
 				if [ "$COUNT" -gt 0 ]; then
