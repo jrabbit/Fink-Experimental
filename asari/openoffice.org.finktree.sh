9cb2a99de5c8f68714e95e8062655706af8d60b1
aida_s <aida_s>
2006-02-12 04:44:10 +0000

    Add to only 10.3's unstable.
    * New variants -firefox and -nocrypto.
    * New command %p/sbin/update-ooo-fonts.
    * Cleanup BuildDepends and Depends.
    * Provides: openoffice.org-generation2.
    * New upstream m156. Maybe run on Intel.

---
+++ asari/openoffice.org.finktree.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+: ${INFOFILE="openoffice.org.info.m4"}
+: ${M4="/usr/bin/m4"}
+myname=`basename $0`
+
+repo=`cat ../../dists/CVS/Repository 2>/dev/null || :`
+if test "x$repo" != "x/cvsroot/fink/dists"; then
+    echo "Not in the fink tree" >&2
+    exit 2
+fi
+set -e -x
+for tree in 10.3; do
+    $M4 -B32768 -S200 -DTREE="[$tree]" "$INFOFILE" > "../../dists/$tree/unstable/crypto/finkinfo/openoffice.org.info"
+    $M4 -B32768 -S200 -DTREE="[$tree]" -DUSE_FIREFOX=1 "$INFOFILE" > "../../dists/$tree/unstable/crypto/finkinfo/openoffice.org-firefox.info"
+    cp openoffice.org.patch "../../dists/$tree/unstable/crypto/finkinfo/openoffice.org.patch"
+    $M4 -B32768 -S200 -DTREE="[$tree]" -DUSE_CRYPTO=0 "$INFOFILE" > "../../dists/$tree/unstable/main/finkinfo/x11/openoffice.org-nocrypto.info"
+    cp openoffice.org.patch "../../dists/$tree/unstable/main/finkinfo/x11/openoffice.org.patch"
+done
