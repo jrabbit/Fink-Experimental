8df654e7c70efbdc3f8d6777d39727c671227759
aida_s <aida_s>
2008-02-27 14:09:59 +0000

    1.5 release
    * sedsrc/update-sys-xinitrc.in: System xinitrc has been replaced
      unconditionally. Fixed. (s/annswer/answer/g)
    * simple/20dispconf.sh: Fixed another typo, which prevents reading
      system-wide .Xresources and .Xmodmap.

--- todai/xinitrc/simple/20dispconf.sh
+++ todai/xinitrc/simple/20dispconf.sh
@@ -1,9 +1,9 @@
 # $Id$
 : ${xinitrc_dispconf_xrdb_enable=YES}
-: ${xinitrc_dispconf_sysresources="$xinitrc_xconfdir/xinit/.Xresources"}
+: ${xinitrc_dispconf_sysresources="$x_xinitdir/.Xresources"}
 : ${xinitrc_dispconf_userresources="$HOME/.Xresources"}
 : ${xinitrc_dispconf_xmodmap_enable=YES}
-: ${xinitrc_dispconf_sysmodmap="$xinitrc_xconfdir/xinit/.Xmodmap"}
+: ${xinitrc_dispconf_sysmodmap="$x_xinitdir/.Xmodmap"}
 : ${xinitrc_dispconf_usermodmap="$HOME/.Xmodmap"}
 
 case "x$xinitrc_dispconf_xrdb_enable" in
