290f4b45e309253e899ed3cc6e19ed2e93f8095a
gecko2 <gecko2>
2009-10-22 20:32:01 +0000

    More portability

--- gecko2/scripts/sync-distfiles-finkinfo.sh
+++ gecko2/scripts/sync-distfiles-finkinfo.sh
@@ -1,58 +1,70 @@
 #!/bin/sh
+###
+# Change this to match your local mirror structur
+###
+GENERATEDIR="/home/f/fink/finkinfo"
+HTTPSNAPDIR="/home/f/fink/infodist"
+LOGDIR="/home/f/fink/log"
+SCRIPTDIR="/home/f/fink/scripts"
+TMPDIR="/home/f/fink/mirwork"
+
+###
+# Change NOTHING below this line
+###
 printf 'Running generate-distfiles-and-finkinfo-mirror.pl...\n'
-~fink/scripts/generate-distfiles-and-finkinfo-mirror.pl
-test -s ~fink/infodist/FORCED && mv ~fink/infodist/FORCED{,.old}
-test -f ~fink/mirwork/FORCE && test -f ~fink/log/change.log && date -u +%s >~fink/infodist/FORCED && rm -f ~fink/infodist/FORCED.old
-if [ \! -s ~fink/mirwork/mirror.lock ]; then
-	printf "\n\n\nLogFile: ~fink/log/mirror.log\n\n"
-	grep -ve 'has not changed' -e 'fetching files for' -e 'exists$'  ~fink/log/mirror.log | sed 's/^/| /'
-	if [ \! -f ~fink/log/change.log ]; then
-		printf '~fink/log/change.log is missing...'
+${SCRIPTDIR}/generate-distfiles-and-finkinfo-mirror.pl
+test -s ${HTTPSNAPDIR}/FORCED && mv ${HTTPSNAPDIR}/FORCED{,.old}
+test -f ${TMPDIR}/FORCE && test -f ${LOGDIR}/change.log && date -u +%s >${HTTPSNAPDIR}/FORCED && rm -f ${HTTPSNAPDIR}/FORCED.old
+if [ \! -s ${TMPDIR}/mirror.lock ]; then
+	printf "\n\n\nLogFile: "${LOGDIR}"/mirror.log\n\n"
+	grep -ve 'has not changed' -e 'fetching files for' -e 'exists$' ${LOGDIR}/mirror.log | sed 's/^/| /'
+	if [ \! -f ${LOGDIR}/change.log ]; then
+		printf ${LOGDIR}'/change.log is missing...\n'
 		exit 0
 	fi
-	if [ -s ~fink/infodist/FORCED ];then
-		echo "10.4/stable/main/foo.info" >~fink/log/change.log
-		echo "10.4/stable/crypto/foo.info" >>~fink/log/change.log
-		echo "10.4/unstable/main/foo.info" >>~fink/log/change.log
-		echo "10.4/unstable/crypto/foo.info" >>~fink/log/change.log
-		rm -f ~fink/mirwork/FORCE
+	if [ -s ${HTTPSNAPDIR}/FORCED ];then
+		echo "10.4/stable/main/foo.info" >${LOGDIR}/change.log
+		echo "10.4/stable/crypto/foo.info" >>${LOGDIR}/change.log
+		echo "10.4/unstable/main/foo.info" >>${LOGDIR}/change.log
+		echo "10.4/unstable/crypto/foo.info" >>${LOGDIR}/log/change.log
+		rm -f ${TMPDIR}/FORCE
 	fi
-	if [ "$(grep -e '.info$' -e '.patch$' ~fink/log/change.log)" != "" ]; then
-		test -s ~fink/infodist/FORCED && printf '\nFORCING INFODIST REBUILD!\n'
-		printf '\nApplying changes in cvs to infodist files:\n'
-		cd ~fink/finkinfo/dists.public/10.4
+	if [ "$(grep -e '.info$' -e '.patch$' "${LOGDIR}"/change.log)" != "" ]; then
+		test -s ${HTTPSNAPDIR}/FORCED && printf '\nFORCING HTTPSNAP REBUILD!\n'
+		printf '\nApplying changes in cvs to httpsnap files:\n'
+		cd ${GENERATEDIR}/dists.public/10.4
 		for release in stable unstable; do
 			for tree in main crypto; do
-				if [ "$(grep 10.4/"${release}"/"${tree}" ~fink/log/change.log)" != "" ]; then
+				if [ "$(grep 10.4/"${release}"/"${tree}" "${LOGDIR}"/change.log)" != "" ]; then
 					printf " Regenerating 10.4-"${release}"-"${tree}" tarball\n"
-					if tar cjph --group 80 --numeric-owner -f ~fink/infodist/10.4-${release}-${tree}.tbz.new ${release}/${tree}; then
-						mv ~fink/infodist/10.4-${release}-${tree}.tbz.new ~fink/infodist/10.4-${release}-${tree}.tbz
+					if tar cjph --group 80 --numeric-owner -f ${HTTPSNAPDIR}/10.4-${release}-${tree}.tbz.new ${release}/${tree}; then
+						mv ${HTTPSNAPDIR}/10.4-${release}-${tree}.tbz.new ${HTTPSNAPDIR}/10.4-${release}-${tree}.tbz
 					else
 						printf " Regenerating of 10.4-"${release}"-"${tree}" tarball FAILED\!\n"
 						exit 0
 					fi
 					printf " Calculating new checksums for 10.4-"${release}"-"${tree}"\n"
 					for check in md5 sha1 sha256; do
-						${check}sum ~fink/infodist/10.4-${release}-${tree}.tbz | cut -f 1 -d " " >~fink/infodist/10.4-${release}-${tree}.tbz.${check}
+						${check}sum ${HTTPSNAPDIR}/10.4-${release}-${tree}.tbz | cut -f 1 -d " " >${HTTPSNAPDIR}/10.4-${release}-${tree}.tbz.${check}
 					done
 					printf " Creating symlinks and timestamps for 10.5-"${release}"-"${tree}" and 10.6-"${release}"-"${tree}"\n"
-					date -u +%s >~fink/infodist/10.4-${release}-${tree}-TIMESTAMP
+					date -u +%s >${HTTPSNAPDIR}/10.4-${release}-${tree}-TIMESTAMP
 					for dist in 10.5 10.6; do
-						ln -sf 10.4-${release}-${tree}.tbz ~fink/infodist/${dist}-${release}-${tree}.tbz
+						ln -sf 10.4-${release}-${tree}.tbz ${HTTPSNAPDIR}/${dist}-${release}-${tree}.tbz
 						for check in md5 sha1 sha256; do
-							ln -sf 10.4-${release}-${tree}.tbz.${check} ~fink/infodist/${dist}-${release}-${tree}.tbz.${check}
+							ln -sf 10.4-${release}-${tree}.tbz.${check} ${HTTPSNAPDIR}/${dist}-${release}-${tree}.tbz.${check}
 						done
-						ln -sf 10.4-${release}-${tree}-TIMESTAMP ~fink/infodist/${dist}-${release}-${tree}-TIMESTAMP
+						ln -sf 10.4-${release}-${tree}-TIMESTAMP ${HTTPSNAPDIR}/${dist}-${release}-${tree}-TIMESTAMP
 					done
 				fi
 			done
 		done
 	else
-		printf '\nNo changes to infodist needed\n'
+		printf '\nNo changes to httpsnap needed\n'
 	fi
 	printf ' Getting TIMESTAMP\n'
-	cp -p ~fink/finkinfo/dists.public/TIMESTAMP ~fink/infodist/TIMESTAMP
+	cp -p ${GENERATEDIR}/dists.public/TIMESTAMP ${HTTPSNAPDIR}/TIMESTAMP
 	printf ' Cleaning up\n'
-	rm -f ~fink/log/change.log
+	rm -f ${LOGDIR}/change.log
 	printf 'done\n'
 fi
