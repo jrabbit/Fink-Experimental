a0893bd682fda4d344779a6675bec6b9ed0b6e3e
gecko2 <gecko2>
2009-10-24 14:55:04 +0000

    changing to internal names

--- gecko2/scripts/sync-info.sh
+++ gecko2/scripts/sync-info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-rsync -4 --address=85.214.23.162 -azv --delete --delete-after --exclude=LOCAL --exclude=TIMESTAMP rsync://distfiles.ber.de.eu.finkmirrors.net/finkinfo ~fink/finkinfo && \
+rsync -4 --address=85.214.23.162 -azv --delete --delete-after --exclude=LOCAL --exclude=TIMESTAMP rsync://fink-dist.gecko.ig42.org/finkinfo ~fink/finkinfo && \
 printf "\n\n" && \
-rsync -4 --address=85.214.23.162 -a --delete --delete-after rsync://distfiles.ber.de.eu.finkmirrors.net/finkinfo/TIMESTAMP ~fink/finkinfo
+rsync -4 --address=85.214.23.162 -a --delete --delete-after rsync://fink-dist.gecko.ig42.org/finkinfo/TIMESTAMP ~fink/finkinfo
 date -u +%s >~fink/finkinfo/LOCAL
