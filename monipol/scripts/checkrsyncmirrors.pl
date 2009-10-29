83bf990a9ccded57cbf83978374e2ffdb6a5de58
monipol <monipol>
2009-10-29 12:51:06 +0000

    Checks the status (TIMESTAMP, LOCAL) of rsync mirrors.

---
+++ monipol/scripts/checkrsyncmirrors.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use POSIX;
+
+use strict;
+use warnings;
+
+my $basepath = '/sw';
+
+my $mirror_FH;
+my $timestamp_FH;
+my $mirror;
+my $timestamp = "";
+my $command;
+my $command_result;
+my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday);
+my $ts;
+my @files = ('TIMESTAMP', 'LOCAL');
+#my @files = ('TIMESTAMP');
+my $file;
+
+open($mirror_FH, '<', "$basepath/lib/fink/mirror/rsync") || die "Could not open rsync mirrors file\n";
+while (<$mirror_FH>) {
+	if($_ =~ /(^...-..: )(rsync:\/\/.*)/) {
+		$mirror = $2;
+		chomp $mirror ;
+		print "$mirror:\n";
+		foreach $file (@files) {
+			system("rm -f ./$file");
+			$command = "rsync -qaz $mirror" . "$file ./$file 2>/dev/null";
+			$command_result = system("$command");
+			$command_result = $command_result >> 8;
+			if ($command_result == 0) {
+				if (open($timestamp_FH, '<', "./$file")) {
+					$timestamp = <$timestamp_FH>;
+					if (defined $timestamp) {
+						chomp $timestamp;
+						close $timestamp_FH;
+						$ts = `date -j -f \%s $timestamp \%+\%`;
+						chomp $ts;
+						print "\t$file = $timestamp = $ts\n";
+					}
+					else {
+						print "\t$file couldn't be read\n";
+					}
+				}
+				else {
+					print "\tCouldn't open $file file\n";
+				}
+			}
+			else {
+				print "\tRsync failed for $file\n";
+			}
+		}
+	}
+}
+close($mirror_FH);
